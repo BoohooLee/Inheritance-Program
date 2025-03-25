64096a08fb9171f556630cc3ff8bfa34584add0d
BoohooLee <111921169+BoohooLee@users.noreply.github.com>
2025-03-25 16:36:21 -0700

    Create PartTimeEmployee.java

---
+++ PartTimeEmployee.java
@@ -0,0 +1,15 @@
+class PartTimeEmployee extends Employee {
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(String name, int id, int hoursWorked, double hourlyRate) {
+        super(name, id, 0); // baseSalary is not relevant here
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hoursWorked * hourlyRate;
+    }
+}
